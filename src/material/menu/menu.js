d0fce5e60d70ba16c522d75d00300842e0eb153a
Ridho Prasetya <ndiing.inc@gmail.com>
2025-01-28 13:40:10 +0700

    fix typo on menu show component

--- src/material/menu/menu.js
+++ src/material/menu/menu.js
@@ -100,7 +100,7 @@ class MdMenuComponent extends MdComponent {
         };
         this.open = true;
         this.popper = new PopperController();
-        this.popover.show(options);
+        this.popper.show(options);
         this.emit("onMenuShown");
     }
 
