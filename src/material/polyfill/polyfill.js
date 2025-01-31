14920ae9d7c9309d9360477fc8b40e2fe04946cf
Ridho Prasetya <ndiing.inc@gmail.com>
2025-01-31 11:41:54 +0700

    clean code

--- src/material/polyfill/polyfill.js
+++ src/material/polyfill/polyfill.js
@@ -1,19 +1,16 @@
-
-Date.prototype.setWeek = function(year, week) {
-    
-    const date = new Date(year, 0, 1);  
-    const days = (week - 1) * 7;  
-    date.setDate(date.getDate() + days);  
-    this.setFullYear(date.getFullYear(), date.getMonth(), date.getDate());  
+Date.prototype.setWeek = function (year, week) {
+    const date = new Date(year, 0, 1);
+    const days = (week - 1) * 7;
+    date.setDate(date.getDate() + days);
+    this.setFullYear(date.getFullYear(), date.getMonth(), date.getDate());
     return this;
 };
 
-Date.prototype.getWeek = function() {
-    
+Date.prototype.getWeek = function () {
     const firstDayOfYear = new Date(this.getFullYear(), 0, 1);
-    
+
     const days = Math.floor((this - firstDayOfYear) / (24 * 60 * 60 * 1000));
-    
+
     const weekNumber = Math.ceil((days + 1) / 7);
     return weekNumber;
 };
